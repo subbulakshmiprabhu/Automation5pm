24511aa45bb42812b5b807eedc6913c6c9d6e83f
subbulakshmmiprabhu <subbulakshmi211@gmail.com>
2024-07-12 16:17:45 +0530

    blank commit

--- src/test/java/basic/NewFile.java
+++ src/test/java/basic/NewFile.java
@@ -4,11 +4,7 @@ public class NewFile{
 
   
 	public static void main(String[] args) {
-           System.out.println("new file added");
-		 System.out.println("again changes from remote");
-		 
-		 System.out.println("new file changed");
-		
+         		
   }
 
 }
--- src/test/java/basic/RemoteFile.java
+++ src/test/java/basic/RemoteFile.java
@@ -5,11 +5,7 @@ public class RemoteFile{
   
 	public static void main(String[] args) {
           
-		 System.out.println("changes from remote");
-
-		  System.out.println("another changes from remote");
-
-		 System.out.println("change from local");
+		 
 		 
 
 		
