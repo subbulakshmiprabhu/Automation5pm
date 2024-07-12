93a78ed92ad9023f530f311a2ea7a633468f3605
subbulakshmmiprabhu <subbulakshmi211@gmail.com>
2024-07-12 16:28:06 +0530

    1commit

--- src/test/java/basic/RemoteFile.java
+++ src/test/java/basic/RemoteFile.java
@@ -5,8 +5,11 @@ public class RemoteFile{
   
 	public static void main(String[] args) {
           
+
 			 System.out.println("remote change");
 	 
+		 System.out.println("local change");
+
 		 
 
 		
